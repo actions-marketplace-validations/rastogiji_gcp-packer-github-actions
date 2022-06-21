862c8d09cfadeebbdedd115b8dee03ff6b0e660c
Animesh Rastogi <animesh.rastogi54@gmail.com>
2022-06-21 18:24:10 +0530

    Test #8

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ set -e
 cd "$INPUT_WORKING_DIRECTORY"
 
 if ( [ ! -f "${INPUT_MAIN_FILE}" ] &&  $INPUT_MAIN_FILE != *.json ] ]); then
-    echo "${INPUT_MAIN_FILE} does not exit in the working directory (${INPUT_WORKING_DIRECTORY})"
+    echo "Could Not find ${INPUT_MAIN_FILE} in (${INPUT_WORKING_DIRECTORY})."
     exit 1
 fi
 
 if [[ ! -f "${INPUT_VARIABLE_FILE}" ]] && [[ $INPUT_VARIABLE_FILE != *.json ]] && [[ $INPUT_VARIABLE_FILE != "" ]]; then
-    echo "$INPUT_VARIABLE_FILE not found in the working directory (${INPUT_WORKING_DIRECTORY})"
+    echo "Could not find $INPUT_VARIABLE_FILE in (${INPUT_WORKING_DIRECTORY})"
     exit 1
 fi
 

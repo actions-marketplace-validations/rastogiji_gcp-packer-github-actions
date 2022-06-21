039d8b44bcd62ce88a77260734b655ca5b4846d2
Animesh Rastogi <animesh.rastogi54@gmail.com>
2022-06-21 19:59:02 +0530

    Test #13

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,24 @@ set -e
 
 cd "$INPUT_WORKING_DIRECTORY"
 
+# Checking whether Main File exists and is a json file
 if [[ ! -f "${INPUT_MAIN_FILE}" ]] && [[ $INPUT_MAIN_FILE != *.json ]]; then
     echo "Could Not find ${INPUT_MAIN_FILE} in (${INPUT_WORKING_DIRECTORY})."
     exit 1
 fi
 
-if ([[ ! -f "${INPUT_VARIABLE_FILE}" ]] && [[ $INPUT_VARIABLE_FILE != *.json ]]) || [[ $INPUT_VARIABLE_FILE != "" ]]; then
+# Checking whether Variable file exists and is a json file
+if [[ ! -f "${INPUT_VARIABLE_FILE}" ]] && [[ $INPUT_VARIABLE_FILE != *.json ]]; then
     echo "Could not find $INPUT_VARIABLE_FILE in (${INPUT_WORKING_DIRECTORY})"
     exit 1
 fi
 
+# Creating -var-file argument if variable file exists
 var_command=""
 if [[ -f "$INPUT_VARIABLE_FILE" ]]; then
     var_command="-var-file=$INPUT_VARIABLE_FILE"
 fi
 
+# Running Packer Build
 echo "Beginning running packer build ..."
 packer build ${var_command} ${INPUT_MAIN_FILE}

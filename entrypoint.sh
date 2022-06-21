de21393d7f5d35eda868283bf29c2d1c87efad3f
Animesh Rastogi <animesh.rastogi54@gmail.com>
2022-06-21 18:35:32 +0530

    Test #9

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$INPUT_WORKING_DIRECTORY"
 
-if ( [ ! -f "${INPUT_MAIN_FILE}" ] &&  $INPUT_MAIN_FILE != *.json ] ]); then
+if [[ ! -f "${INPUT_MAIN_FILE}" ]] && [[ $INPUT_MAIN_FILE != *.json ]]; then
     echo "Could Not find ${INPUT_MAIN_FILE} in (${INPUT_WORKING_DIRECTORY})."
     exit 1
 fi
@@ -14,7 +14,7 @@ if [[ ! -f "${INPUT_VARIABLE_FILE}" ]] && [[ $INPUT_VARIABLE_FILE != *.json ]] &
 fi
 
 var_command=""
-if [ -f "$INPUT_VARIABLE_FILE" ]; then
+if [[ -f "$INPUT_VARIABLE_FILE" ]]; then
     var_command="-var-file=$INPUT_VARIABLE_FILE"
 fi
 

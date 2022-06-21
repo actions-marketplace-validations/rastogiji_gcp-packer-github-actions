48ca550f532e783995d1f019e0a3886b59dc60eb
Animesh Rastogi <animesh.rastogi54@gmail.com>
2022-06-21 20:10:25 +0530

    Test #14

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM hashicorp/packer:latest
+LABEL Author="Animesh Rastogi"
 
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,18 +9,15 @@ if [[ ! -f "${INPUT_MAIN_FILE}" ]] && [[ $INPUT_MAIN_FILE != *.json ]]; then
     exit 1
 fi
 
+var_command=""
+
 # Checking whether Variable file exists and is a json file
 if [[ ! -f "${INPUT_VARIABLE_FILE}" ]] && [[ $INPUT_VARIABLE_FILE != *.json ]]; then
     echo "Could not find $INPUT_VARIABLE_FILE in (${INPUT_WORKING_DIRECTORY})"
     exit 1
-fi
-
-# Creating -var-file argument if variable file exists
-var_command=""
-if [[ -f "$INPUT_VARIABLE_FILE" ]]; then
+else
     var_command="-var-file=$INPUT_VARIABLE_FILE"
 fi
 
 # Running Packer Build
-echo "Beginning running packer build ..."
 packer build ${var_command} ${INPUT_MAIN_FILE}
